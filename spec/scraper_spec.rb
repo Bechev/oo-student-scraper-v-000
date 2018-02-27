02144399d718ef9070dfee33648595338c5b8a54
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-27 02:25:06 +0000

    automatically backed up by learn

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -38,6 +38,7 @@ describe "Scraper" do
       scraped_student = Scraper.scrape_profile_page(profile_url)
       expect(scraped_student).to be_a(Hash)
       expect(scraped_student).to match(student_joe_hash)
+
     end
 
     it "can handle profile pages without all of the social links" do
@@ -45,4 +46,4 @@ describe "Scraper" do
       scraped_student = Scraper.scrape_profile_page(profile_url)
       expect(scraped_student).to be_a(Hash)
       expect(scraped_student).to match(student_david_hash)
-  
+    en
