5a31e0166ad1fbd3ec6eb99bff3f580c9f3d55d1
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-27 02:23:19 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -39,11 +39,11 @@ class Scraper
       else
         student_profile[:blog] = link
       end
+      binding.pry
     end
     student_profile[:profile_quote] = doc.css(".profile-quote").text if doc.css(".profile-quote")
     student_profile[:bio] = doc.css("div.bio-content.content-holder div.description-holder p").text if doc.css("div.bio-content.content-holder div.description-holder p")
     student
-    binding.pry
   end
 
 end
